72037acc1e5bc803960da68c82b395d8ae103b4d
MhN_AR <mahan85a@gmail.com>
2026-08-03 15:30:13 +0330

    feat: add BaseModel class

---
+++ src/main/java/ir/maktabsharif/model/BaseModel.java
@@ -0,0 +1,21 @@
+package ir.maktabsharif.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseModel<ID> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+}
