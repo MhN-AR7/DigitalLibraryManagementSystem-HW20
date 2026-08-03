a54f4198d64494a06c1243687819006a88cbd1b8
MhN_AR <mahan85a@gmail.com>
2026-08-03 15:31:43 +0330

    feat: add GenericRepo Interface

---
+++ src/main/java/ir/maktabsharif/repository/GenericRepo.java
@@ -0,0 +1,12 @@
+package ir.maktabsharif.repository;
+
+import ir.maktabsharif.model.BaseModel;
+
+import java.util.Optional;
+
+public interface GenericRepo<T extends BaseModel<ID>, ID> {
+    void insert(T t);
+    Optional<T> findById(ID id);
+    boolean update(T t);
+    boolean delete(ID id);
+}
