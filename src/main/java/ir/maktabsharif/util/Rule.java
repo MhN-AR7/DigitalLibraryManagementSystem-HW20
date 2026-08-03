46651a7a84ed17983610e94725ecac4d75d7c24e
MhN_AR <mahan85a@gmail.com>
2026-08-03 15:32:42 +0330

    feat: add Rule util

---
+++ src/main/java/ir/maktabsharif/util/Rule.java
@@ -0,0 +1,18 @@
+package ir.maktabsharif.util;
+
+import ir.maktabsharif.exception.BusinessException;
+
+import java.util.function.Function;
+
+public class Rule {
+    private Rule() {
+    }
+
+    public static void check (
+            boolean condition,
+            Function<String, ? extends BusinessException> exceptionFunction,
+            String message
+    ) throws BusinessException {
+        if (condition) throw exceptionFunction.apply(message);
+    }
+}
