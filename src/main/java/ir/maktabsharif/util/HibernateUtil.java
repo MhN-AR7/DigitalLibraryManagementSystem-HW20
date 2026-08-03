fd604855f9d9fbd9e93d3df9523b3333ba44de10
MhN_AR <mahan85a@gmail.com>
2026-08-03 15:31:24 +0330

    feat: add HibernateUtil

---
+++ src/main/java/ir/maktabsharif/util/HibernateUtil.java
@@ -0,0 +1,42 @@
+package ir.maktabsharif.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.function.Function;
+
+public class HibernateUtil {
+    private static final String PERSISTENCE_UNIT = "postgres-pu";
+
+    private static EntityManagerFactory emf;
+
+    private static EntityManagerFactory getEmf() {
+        if (emf == null) emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+        return emf;
+    }
+
+    public static void closeEmf() {
+        if (emf != null && emf.isOpen()) emf.close();
+    }
+
+    private static EntityManager getEm() {
+        return getEmf().createEntityManager();
+    }
+
+    public static <T> T inTxResult(Function<EntityManager, T> operation) {
+        EntityManager em = getEm();
+        EntityTransaction tx = null;
+        try (em) {
+            tx = em.getTransaction();
+            tx.begin();
+            T result = operation.apply(em);
+            tx.commit();
+            return result;
+        } catch (RuntimeException e) {
+            if (tx != null && tx.isActive()) tx.rollback();
+            throw e;
+        }
+    }
+}
